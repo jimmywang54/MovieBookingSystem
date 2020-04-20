63a8550e4a2a052a517641c4ef72d8726ceb696a
PinHoWang <abc8755987@gmail.com>
2020-04-20 19:18:02 -0400

    Added mobile version

---
+++ movie-frontend-mobile/www/js/precache-manifest.73f37ccf3d166240cf320fd26621ab90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badebb0a45015bb3e2b284321c421af6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20bd494b5d2678ad1f71",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f52f1706e6b84d08c6f7",
+    "url": "/static/css/main.d6961b92.chunk.css"
+  },
+  {
+    "revision": "20bd494b5d2678ad1f71",
+    "url": "/static/js/2.efc12568.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.efc12568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52f1706e6b84d08c6f7",
+    "url": "/static/js/main.620d5c9e.chunk.js"
+  },
+  {
+    "revision": "3e3c68619b3fb9055d0d",
+    "url": "/static/js/runtime-main.e7409146.js"
+  }
+]);
